70d12359675242cbd247fb01d0b671bdf2573f94
Viacheslav-ua <atph@ukr.net>
2024-01-06 15:44:31 +0200

    fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from 'next-auth'
-import { authOptions } from '@/shared/options/auth'
+import { authOptions } from '@/shared/options/auth-options'
 
 const handler = NextAuth(authOptions)
 export {handler as GET, handler as POST}
